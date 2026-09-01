7dc63042335d32a6006a9479e6a84f802ec37e32
Steven Diamante <stevendiamante13@gmail.com>
2026-09-01 09:35:45 -0400

    r add failing repeated search dedupe test

--- src/utils/searchHistory.test.ts
+++ src/utils/searchHistory.test.ts
@@ -9,4 +9,8 @@ describe('recordSearch', () => {
   test('puts the newest search at the top', () => {
     expect(recordSearch([apple], microsoft)).toEqual([microsoft, apple]);
   });
+
+  test('moves a repeated search to the top instead of duplicating it', () => {
+    expect(recordSearch([microsoft, apple], apple)).toEqual([apple, microsoft]);
+  });
 });
